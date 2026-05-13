32ac8d7a449b61a896df6dd4c78557a2922ea15c
Ganesh401697 <gangisettyganesh59@gmail.com>
2026-05-13 20:25:52 +0530

    Add files via upload

---
+++ Day02_python_basics.py
@@ -0,0 +1,45 @@
+# Integer Variables
+age = 20
+marks = 95
+students = 45
+
+# Float Variables
+height = 5.8
+temperature = 36.5
+price = 199.99
+
+# String Variables
+name = "Ganesh"
+city = "Hyderabad"
+course = "Python"
+
+# Boolean Variables
+is_student = True
+has_laptop = False
+is_learning = True
+
+# More Variables
+favorite_color = "Blue"
+salary = 25000
+percentage = 89.5
+
+# Print Variables
+print(age)
+print(marks)
+print(students)
+
+print(height)
+print(temperature)
+print(price)
+
+print(name)
+print(city)
+print(course)
+
+print(is_student)
+print(has_laptop)
+print(is_learning)
+
+print(favorite_color)
+print(salary)
+print(percentage)
